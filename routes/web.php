398855e3bf490dd34affce852c2ed6f36c4e6b2d
ilmaap <ilmaapsputri@gmail.com>
2024-05-07 14:25:18 +0700

    lanjut_regis

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,10 @@ Route::get('/', function () {
 });
 
 
-<<<<<<< HEAD
+
 Route::get('/register1', [RegistersatuController::class, 'regis1']);
 Route::get('/register2', [RegistersatuController::class, 'regis2']);
 Route::get('/register3', [RegistersatuController::class, 'regis3']);
-=======
-Route::get('/register1', [RegisterController::class, 'regis1']);
-Route::get('/register', [RegisterController::class, 'index']);
-Route::get('/registertiga', [RegistertigaController::class, 'index']);
->>>>>>> c197f62a6c2b85bacf371c0fa5f8a9a4e04ece55
+
 Route::get('/ranap',[RanapController::class, 'index']);
 Route::get('/form_add', [Form_Add_DataController::class, 'index']);
